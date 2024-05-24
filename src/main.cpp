15ce41f7616c384bf7d60cab5a9e0a62e4900639
Okabe Minato <166805519+hou-rai3@users.noreply.github.com>
2024-05-24 18:40:44 +0900

    Update main.cpp
    割り込みを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,70 @@
 #include "mbed.h"
+
 uint8_t DATA_1[8] = {};
 uint8_t DATA_2[8] = {};
+uint8_t DATA_3[8] = {};
+InterruptIn button(D4);          // リミットスイッチ
+InterruptIn button_reset(PC_13); // 起動スイッチ
+BufferedSerial pc(USBTX, USBRX, 115200);
+CAN can(PA_11, PA_12, (int)1e6);
 
-int main()
+int16_t motor_1 = 0;
+int16_t motor_2 = 0;
+int16_t motor_3 = 0;
+int16_t speed = 30000; // 16ビットの最大値
+
+int reset;
+int sw;
+
+void Switch_Reset()
 {
+    printf("Restart\n");
+    motor_1 = speed;
+    motor_2 = speed;
+    int16_t output_1 = motor_1;
+    int16_t output_2 = motor_2;
+    int16_t output_3 = motor_3;
+    DATA_1[0] = -output_1 & 0xFF;        // 下位バイト
+    DATA_1[1] = (-output_1 >> 8) & 0xFF; // 上位バイト
+    DATA_2[2] = output_2 & 0xFF;         // 下位バイト
+    DATA_2[3] = (output_2 >> 8) & 0xFF;  // 上位バイト
+    DATA_3[4] = output_3 & 0xFF;         // 下位バイト
+    DATA_3[5] = (output_3 >> 8) & 0xFF;  // 上位バイト
+}
 
-    auto now = HighResClock::now();
-    static auto pre = now;
-
-    DigitalIn button(D4); // リミットスイッチ
-    BufferedSerial pc(USBTX, USBRX, 115200);
-    CAN can(PA_11, PA_12, (int)1e6);
-    button.mode(PullUp);
-
-    int16_t motor_1 = 0;
-    int16_t motor_2 = 0;
-    int16_t speed = 30000; // 16ビットの最大値
-
+void Switch_Stop()
+{
+    printf("Stop\n");
+    int16_t speed = 0;
     motor_1 = speed;
     motor_2 = speed;
 
     int16_t output_1 = motor_1;
+    int16_t output_2 = motor_2;
+    int16_t output_3 = motor_3;
     DATA_1[0] = output_1 & 0xFF;        // 下位バイト
     DATA_1[1] = (output_1 >> 8) & 0xFF; // 上位バイト
+    DATA_2[2] = output_2 & 0xFF;        // 下位バイト
+    DATA_2[3] = (output_2 >> 8) & 0xFF; // 上位バイト
+    DATA_3[4] = output_3 & 0xFF;        // 下位バイト
+    DATA_3[5] = (output_3 >> 8) & 0xFF; // 上位バイト
+}
 
-    int16_t output_2 = motor_2;
-    DATA_2[0] = output_2 & 0xFF;        // 下位バイト
-    DATA_2[1] = (output_2 >> 8) & 0xFF; // 上位バイ
+int main()
+{
+    button.mode(PullUp);
+    button_reset.mode(PullUp);
+
+    button_reset.fall(&Switch_Reset);
+    button.fall(&Switch_Stop);
+
     while (1)
     {
-        int sw = button.read(); // スイッチの状態を読み取る
-        CANMessage msg0(0x1FF, DATA_1, 8);
+        CANMessage msg0(0x200, DATA_1, 8);
         CANMessage msg1(0x200, DATA_2, 8);
+        CANMessage msg2(0x200, DATA_3, 8);
         can.write(msg0);
         can.write(msg1);
-        if (sw == 0)
-        {
-            int16_t speed = 0;
-            motor_1 = speed;
-            motor_2 = speed;
-
-            int16_t output_1 = motor_1;
-            DATA_1[0] = output_1 & 0xFF;        // 下位バイト
-            DATA_1[1] = (output_1 >> 8) & 0xFF; // 上位バイト
-
-            int16_t output_2 = motor_2;
-            DATA_2[0] = output_2 & 0xFF;        // 下位バイト
-            DATA_2[1] = (output_2 >> 8) & 0xFF; // 上位バイ
-        }
-        if (now - pre > 100ms)
-        {
-            pre = now;
-        }
+        can.write(msg2);
     }
 }
