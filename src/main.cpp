c4da96209db1c2b5630fa26737bf60d3bdf2b95d
Okabe Minato <166805519+hou-rai3@users.noreply.github.com>
2024-07-06 15:14:17 +0900

    割り込み

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,11 @@ FirstPenguin penguin_2(FP_2, can);
 
 DigitalIn encoderA(PB_13); // A相のピン A0からA5
 DigitalIn encoderB(PB_14); // B相のピン A0からA5
+int counter = 0;
 
 void stop_motor(int speed)
 {
-    printf("STOP\n");
+    printf("停止\n");
 
     penguin_1.pwm[0] = speed;
     penguin_1.pwm[1] = speed;
@@ -28,7 +29,7 @@ void stop_motor(int speed)
 
 void slowmove(int speed)
 {
-    printf("slowmove Ready\n");
+    printf("低速動作\n");
 
     penguin_1.pwm[0] = speed;
     penguin_1.pwm[1] = speed;
@@ -39,10 +40,11 @@ void slowmove(int speed)
     penguin_2.pwm[1] = -speed;
 }
 
-void button_stop_pressed()
+bool button_stop_pressed()
 {
-    printf("button Read");
+    printf("停止スイッチ確認\n");
     stop_motor(0);
+    return true;
 }
 
 int main()
@@ -51,63 +53,57 @@ int main()
     InterruptIn button_stop(PC_8);
     button_reset.mode(PullUp);
     button_stop.mode(PullUp);
+    bool flag_reset = false;
+    bool slow_move_started = false; // slowmoveが開始されたかどうかを追跡
 
-    button_stop.fall(&button_stop_pressed); // button_stopが押された時にstop_motorを実行
+    button_stop.fall([&]
+                     { flag_reset = button_stop_pressed(); }); // button_stopが押された時にstop_motorを実行
 
     auto pre = HighResClock::now();
-    auto pre_1 = pre;
     auto slow_move_start_time = HighResClock::now(); // slowmoveを開始する時間を追跡
-    bool slow_move_started = false;                  // slowmoveが開始されたかどうかを追跡
+
+    int speed = 0;
+    int Count_Limit = -2700;
     bool A_pre = 0;
     bool B_pre = 0;
-    int counter = 0;
-    int speed = 0;
-    int Count_Limit = -60150;
-    bool flag_reset = false;
 
     while (1)
     {
         auto now = HighResClock::now();
-        auto now_1 = HighResClock::now();
         bool A_now = encoderA.read();
         bool B_now = encoderB.read();
 
         counter = launcher_counter(A_pre, B_pre, A_now, B_now, counter);
-
         A_pre = A_now;
         B_pre = B_now;
-
-        if (now - pre > 500ms && !button_stop.read())
+        if (flag_reset && !slow_move_started && now - pre > 1000ms)
         {
-            stop_motor(0);
-            flag_reset = true;
-            counter = 0;
-            // slowmoveを開始する時間を設定
-            slow_move_start_time = HighResClock::now();
+            slowmove(2000);
             slow_move_started = true;
-
-            pre = now;
+            slow_move_start_time = HighResClock::now();
+            flag_reset = false;
         }
 
-        // slowmoveが開始されてから100ms経過した場合にslowmoveを実行
+        // slowmoveが開始されてから1000ms経過した場合にslowmoveを実行
         if (slow_move_started && now - slow_move_start_time > 1000ms)
         {
             slowmove(2000);
-
             slow_move_started = false; // slowmoveの実行後はフラグをリセット
         }
-        if (counter < Count_Limit && flag_reset)
+
+        if (counter < Count_Limit && slow_move_started)
         {
             printf("count\n");
             stop_motor(0);
             counter = 0;
             flag_reset = false;
         }
+
         if (now - pre > 1000ms && !button_reset.read())
         {
-            printf("FIRE\n");
+            printf("発射\n");
             speed = 30000;
-
+            counter = 0;
             penguin_1.pwm[0] = speed;
             penguin_1.pwm[1] = speed;
             penguin_1.pwm[2] = speed;
@@ -119,17 +115,17 @@ int main()
             pre = now;
         }
 
-        if (now_1 - pre_1 > 10ms)
+        if (now - pre > 10ms)
         {
             if (penguin_1.send() && penguin_2.send())
             {
-                printf("counter=%d: can OK\n", counter);
+                printf("counter = %d: can OK\n", counter);
             }
             else
             {
-                printf("counter=%d: can't Message\n", counter);
+                printf("counter = %d: can't Message\n", counter);
             }
-            pre_1 = now_1;
+            pre = now;
         }
     }
 }
