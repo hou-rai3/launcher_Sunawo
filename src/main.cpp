612d1aa4b331205e81bb5ef83cc57a69686b7565
Okabe Minato <166805519+hou-rai3@users.noreply.github.com>
2024-09-02 11:15:03 +0900

    アウトリガー削除/冗長性減少

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "FP.hpp"
 #include "sengen.hpp"
 
+bool Higher_lock = true;
+bool Lowre_lock = true;
+
 void motor_move(int speed)
 {
     penguin_1.pwm[1] = -speed;
@@ -18,21 +21,12 @@ void fire_under(int speed)
 
 void handle_higher()
 {
-    if (!Flags::Higher_stop)
-    {
-        ints::Higher_count++;
-    }
-    else
-    {
-        ints::Higher_count--;
-    }
-
+    ints::Higher_count += Flags::Higher_stop ? -1 : 1;
     ints::Higher_count = clamp(ints::Higher_count, 0, 2000);
     if (ints::Higher_count == 600 && !Flags::Higher_flag)
     {
         Flags::Higher_flag = true;
         motor_move(3000);
-        // printf("Higher_Reload\n");
     }
     if (ints::Higher_count == 0)
     {
@@ -42,21 +36,12 @@ void handle_higher()
 
 void handle_lower()
 {
-    if (!Flags::Lower_stop)
-    {
-        ints::Lower_count++;
-    }
-    else
-    {
-        ints::Lower_count--;
-    }
-
+    ints::Lower_count += Flags::Lower_stop ? -1 : 1;
     ints::Lower_count = clamp(ints::Lower_count, 0, 1000);
     if (ints::Lower_count == 600 && !Flags::Lower_flag)
     {
         Flags::Lower_flag = true;
         fire_under(3000);
-        // printf("Lower_Reload\n");
     }
     if (ints::Lower_count == 0)
     {
@@ -66,21 +51,13 @@ void handle_lower()
 
 void higher_reload()
 {
-    if (Flags::Higher_Reload)
-    {
-        ints::Higher_Reload++;
-    }
-    else
-    {
-        ints::Higher_Reload--;
-    }
-
+    ints::Higher_Reload += Flags::Higher_Reload ? 1 : -1;
     ints::Higher_Reload = clamp(ints::Higher_Reload, 0, 500);
     if (ints::Higher_Reload == 200 && !Flags::Higher_Reload)
     {
         Flags::Higher_Reload = true;
         motor_move(0);
-        // printf("Higher_Reload\n");
+        Higher_lock = true;
     }
     if (ints::Higher_Reload == 0)
     {
@@ -90,21 +67,13 @@ void higher_reload()
 
 void lower_reload()
 {
-    if (!Flags::Lower_Reload)
-    {
-        ints::Lower_Reload++;
-    }
-    else
-    {
-        ints::Lower_Reload--;
-    }
-
+    ints::Lower_Reload += Flags::Lower_Reload ? 1 : -1;
     ints::Lower_Reload = clamp(ints::Lower_Reload, 0, 100);
     if (ints::Lower_Reload == 50 && !Flags::Lower_Reload)
     {
         Flags::Lower_Reload = true;
         fire_under(0);
-        // printf("Lower_Reload\n");
+        Lowre_lock = false;
     }
     if (ints::Lower_Reload == 0)
     {
@@ -114,21 +83,12 @@ void lower_reload()
 
 void higher_fire()
 {
-    if (Flags::Higher_fire)
-    {
-        ints::Higher_fire++;
-    }
-    else
-    {
-        ints::Higher_fire--;
-    }
-
+    ints::Higher_fire += Flags::Higher_fire ? 1 : -1;
     ints::Higher_fire = clamp(ints::Higher_fire, 0, 100);
     if (ints::Higher_fire == 50 && !Flags::Higher_fire)
     {
         Flags::Higher_fire = true;
         motor_move(ints::Higher_speed);
-        // printf("Higher_fire\n");
     }
     if (ints::Higher_fire == 0)
     {
@@ -138,21 +98,12 @@ void higher_fire()
 
 void lower_fire()
 {
-    if (!Flags::Lower_fire)
-    {
-        ints::Lower_fire++;
-    }
-    else
-    {
-        ints::Lower_fire--;
-    }
-
+    ints::Lower_fire += Flags::Lower_fire ? 1 : -1;
     ints::Lower_fire = clamp(ints::Lower_fire, 0, 150);
     if (ints::Lower_fire == 100 && !Flags::Lower_fire)
     {
         Flags::Lower_fire = true;
         fire_under(ints::Lower_speed);
-        // printf("Lower_fire\n");
     }
     if (ints::Lower_fire == 0)
     {
@@ -166,8 +117,6 @@ void read_stop()
     handle_lower();
     higher_reload();
     lower_reload();
-    // higher_fire();
-    // lower_fire();
 }
 
 void read_swich()
@@ -182,22 +131,25 @@ void read_swich()
 
 void Setup()
 {
-    Buttons::Higher_fire.mode(PullUp);
-    Buttons::Lower_fire.mode(PullUp);
-    Buttons::Higher_stop.mode(PullUp);
-    Buttons::Lower_stop.mode(PullUp);
-    Buttons::Higher_Reload.mode(PullUp);
-    Buttons::Lower_Reload.mode(PullUp);
+    Higher_lock = true;
+    Lowre_lock = true;
+    InterruptIn *buttons[] = {
+        &Buttons::Higher_fire, &Buttons::Lower_fire, &Buttons::Higher_stop, &Buttons::Lower_stop,
+        &Buttons::Higher_Reload, &Buttons::Lower_Reload};
+    for (auto button : buttons)
+    {
+        button->mode(PullUp);
+    }
+    // std::fill(std::begin(pwm), std::end(pwm), 0);
 }
 
 void controler()
-
 {
     if (can.read(msg) && msg.id == 0x7ff)
     {
-        Data[0] = (msg.data[0] >> 7) & 0x01; // circle
-        Data[1] = (msg.data[1] >> 7) & 0x01; // cross
-        Data[2] = (msg.data[2] >> 7) & 0x01; // rectangle
+        Data[0] = (msg.data[0] >> 7) & 0x01;
+        Data[1] = (msg.data[1] >> 7) & 0x01;
+        Data[2] = (msg.data[2] >> 7) & 0x01;
     }
 }
 
@@ -209,33 +161,38 @@ int main()
         read_swich();
         read_stop();
         controler();
-        
         auto now = HighResClock::now();
         auto now_1 = HighResClock::now();
 
-        if (now - pre > 1000ms && Data[0] == 1) // 上発射
+        if (now - pre > 1000ms && Data[0] == 1 && Higher_lock)
         {
+            Higher_lock = false;
             motor_move(ints::Higher_speed);
             pre = now;
         }
-        if (now - pre > 500ms && Data[2] == 1) // 下発射
+        if (now - pre > 500ms && Data[2] == 1 && Lowre_lock)
         {
+            Lowre_lock = false;
             fire_under(ints::Lower_speed);
             pre = now;
         }
-        if (now - pre > 1000ms && !Flags::Higher_fire) // 上発射
+        if (now - pre > 1000ms && !Flags::Higher_fire && Higher_lock)
         {
+            Higher_lock = false;
             motor_move(ints::Higher_speed);
             pre = now;
         }
-        if (now - pre > 500ms && !Flags::Lower_fire) // 下発射
+        if (now - pre > 500ms && !Flags::Lower_fire && Lowre_lock)
         {
+            Lowre_lock = false;
             fire_under(ints::Lower_speed);
             pre = now;
         }
 
         if (now_1 - pre_1 > 30ms)
         {
+            CANMessage msg1(1, (const uint8_t *)pwm, 8);
+            can.write(msg1);
             if (penguin_1.send() && penguin_2.send())
             {
                 printf("can OK \n");
@@ -251,9 +208,6 @@ int main()
                     printf("penguin_2 send failed\n");
                 }
                 printf("can't Message\n");
-
-                // CAN通信のリセットを試みる
-                // can.reset();
             }
             pre_1 = now_1;
         }
